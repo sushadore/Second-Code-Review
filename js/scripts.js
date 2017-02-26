f39abb8e2b05e21c8c572b9e6d1d314c97218397
Susha Dore <susha.dore@gmail.com>
2017-02-26 15:00:46 -0800

    add hide functions to branches so that only one destination shows at at time each time the show button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,23 @@ $(function() {
 
     if (temperature === "warm" && passport === "yes") {
     $("#cabo").show();
+    $("#neworleans").hide();
+    $("#london").hide();
+    $("#parkcity").hide();
   } else if (temperature === "warm" && passport === "no") {
+    $("#cabo").hide();
     $("#neworleans").show();
+    $("#london").hide();
+    $("#parkcity").hide();
   } else if (temperature === "cool" && passport === "yes") {
+    $("#cabo").hide();
+    $("#neworleans").hide();
     $("#london").show();
+    $("#parkcity").hide();
   } else if (temperature === "cool" && passport === "no") {
+    $("#cabo").hide();
+    $("#neworleans").hide();
+    $("#london").hide();
     $("#parkcity").show();
   }
   });
