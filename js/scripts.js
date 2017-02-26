1bc35d2acf9d661796a2a554a4c52cfcb74e6172
Susha Dore <susha.dore@gmail.com>
2017-02-26 15:26:51 -0800

    add functionality to adventure button to always return parkcity if checked

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,40 @@ $(function() {
     $("#survey-hidden").hide();
     $("button#survey").show();
 
-    if (temperature === "warm" && passport === "yes") {
-    $("#cabo").show();
-    $("#neworleans").hide();
-    $("#london").hide();
-    $("#parkcity").hide();
-  } else if (temperature === "warm" && passport === "no") {
-    $("#cabo").hide();
-    $("#neworleans").show();
-    $("#london").hide();
-    $("#parkcity").hide();
-  } else if (temperature === "cool" && passport === "yes") {
-    $("#cabo").hide();
-    $("#neworleans").hide();
-    $("#london").show();
-    $("#parkcity").hide();
-  } else if (temperature === "cool" && passport === "no") {
-    $("#cabo").hide();
-    $("#neworleans").hide();
-    $("#london").hide();
-    $("#parkcity").show();
-  }
+    if (activity === "adventure" && temperature === "cool" && passport === "yes") {
+      $("#cabo").hide();
+      $("#neworleans").hide();
+      $("#london").hide();
+      $("#parkcity").show();
+    } else if (temperature === "warm" && passport === "yes") {
+      $("#cabo").show();
+      $("#neworleans").hide();
+      $("#london").hide();
+      $("#parkcity").hide();
+    } else if (temperature === "warm" && passport === "no") {
+      $("#cabo").hide();
+      $("#neworleans").show();
+      $("#london").hide();
+      $("#parkcity").hide();
+    } else if (temperature === "cool" && passport === "yes") {
+      $("#cabo").hide();
+      $("#neworleans").hide();
+      $("#london").show();
+      $("#parkcity").hide();
+    } else if (temperature === "cool" && passport === "no") {
+      $("#cabo").hide();
+      $("#neworleans").hide();
+      $("#london").hide();
+      $("#parkcity").show();
+    }
   });
 
   $("button#survey").click(function() {
     $("#survey-hidden").show();
     $("button#survey").hide();
+    $("#cabo").hide();
+    $("#neworleans").hide();
+    $("#london").hide();
+    $("#parkcity").hide();
   });
 });
